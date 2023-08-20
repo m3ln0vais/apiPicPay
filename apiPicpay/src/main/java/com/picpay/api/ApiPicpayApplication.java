2d04f706f74f6c8369984af6944ea79fbdf72df2
m3ln0vais <mellnovais14@gmail.com>
2023-08-19 22:57:31 -0300

    first commit

---
+++ apiPicpay/src/main/java/com/picpay/api/ApiPicpayApplication.java
@@ -0,0 +1,13 @@
+package com.picpay.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPicpayApplication.class, args);
+	}
+
+}
---
+++ apiPicpay/src/test/java/com/picpay/api/ApiPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpay.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
